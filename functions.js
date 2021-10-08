f8a6998b8d6557334a46f4cfc2533941ae0aed7a
ArjunGTX <arjunatpavilion@gmail.com>
2021-10-08 20:18:17 +0530

    added function

---
+++ functions.js
@@ -0,0 +1,32 @@
+// Given a sentence, your functions should return the number of words in the sentence.
+
+// function numberOfWords(sentence) {
+//     let trimmedSentence = sentence.trim();
+//     if(trimmedSentence === null || trimmedSentence === "") {
+//         return 0;
+//     } else {
+//         let wordCount = 1;
+//         for(let i=0; i<trimmedSentence.length; i++) {
+//             if(trimmedSentence[i] === ' ') {
+//                 wordCount++
+//             }
+//         }
+//         return wordCount;
+//     }
+   
+// }
+// console.log(numberOfWords('hey there'));
+
+
+
+
+// Given an array, your function should return the length of the array.
+
+// function arrayLength(arr) {
+//     let len = 0
+//     for(let i=0; i<5; i++) {
+//         len++;
+//     }
+//     return len;
+// }
+// console.log(arrayLength([1,2,3,4,5]));
