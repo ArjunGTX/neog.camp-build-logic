ec698cb825c142e162dd2b16869345b5e277ca80
ArjunGTX <arjunatpavilion@gmail.com>
2021-10-12 21:54:11 +0530

    Added 3 string questions
    added solutions of 3 questions from beginner to intermediate

---
+++ strings.js
@@ -0,0 +1,59 @@
+// Program that reads string and count number of characters present in the string
+
+// function getStrLength(str) {
+//     let len = 0;
+//     for(let char of str) {
+//         len++
+//     }
+//     return len;
+// }
+// console.log(getStrLength('hello'));
+
+
+
+
+
+
+
+// Write a program to delete all vowels from a string. Assume string is not more than 80 characters long
+
+// function removeVowels(str) {
+//     let removedStr = str.replaceAll(/[aeiou]/ig,'')
+//     return removedStr;
+// }
+// console.log(removeVowels('hai hEllo'));
+
+
+
+
+
+
+// A program that reads three strings and prints the longest and smallest one
+
+// function getLongestStr(...strings) {
+//     let maxStr = strings[0];
+//     for(let str of strings) {
+//         if(str.length > maxStr.length) {
+//             maxStr = str;
+//         }
+//     }
+//     return maxStr;
+// } 
+// function getSmallestStr(...strings) {
+//     let minStr = strings[0];
+//     for(let str of strings) {
+//         if(str.length < minStr.length) {
+//             minStr = str;
+//         }
+//     }
+//     return minStr;
+// } 
+// console.log(getLongestStr('hi','hello','how are you'));
+// console.log(getSmallestStr('hi','hello','how are you'));
+
+
+
+
+
+
+
