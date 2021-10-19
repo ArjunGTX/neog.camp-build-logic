fd32f9f231bbe91006ca1ed5c7f18a02e7589548
ArjunGTX <arjunatpavilion@gmail.com>
2021-10-19 22:17:05 +0530

    added 1 question to arrays beginner

---
+++ arrays.js
@@ -0,0 +1,21 @@
+// Find sum of an array and display the output . Example [10,4,5,2,5,6,9].
+// Find average of an array and display the output.
+
+// function getSum(arr) {
+//     let sum = 0;
+//     let avg;
+    
+//     for(let num of arr) {
+//         sum += num;
+//     }
+//     return sum;
+// }
+// console.log(getSum([1,2,3,4,5]));
+
+
+
+
+
+
+
+
