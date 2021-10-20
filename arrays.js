7785aed448f740c1ac1f4ec1eda6e998c3ccf84f
ArjunGTX <arjunatpavilion@gmail.com>
2021-10-20 21:04:30 +0530

    added 2 questions to array advanced

--- arrays.js
+++ arrays.js
@@ -19,3 +19,50 @@
 
 
 
+// Find maximum and minimum of an array.
+
+// function getMinMax(arr) {
+//     let max = min = arr[0];
+//     for(let num of arr) {
+//         if(num > max) {
+//             max = num;
+//         }
+//         if(num < min) {
+//             min = num;
+//         }
+//     }
+//     return [min,max];
+// }
+// console.log(getMinMax([1,2,3,4,5]));
+
+
+
+
+
+
+
+// Find Median and Mode of an array.
+// Median : (N+1/2)th term.
+// Mode : Most repeating term
+
+// function medianMode(arr) {
+//     const countObj = {};
+//     let median = arr[((arr.length + 1) / 2) - 1];
+//     for(let i=0; i<arr.length; i++) {
+//         if(countObj.hasOwnProperty(arr[i])) {
+//             countObj[arr[i]] += 1; 
+//         } else {
+//             countObj[arr[i]] = 1;
+//         }
+//     }
+//     let maxCount = 0;
+//     let mode = arr[0];
+//     for(let num in countObj) {
+//         if(countObj[num] > maxCount ) {
+//             maxCount = countObj[num];
+//             mode = Number(num);
+//         }
+//     }
+//     return [median,mode]
+// }
+// console.log(medianMode([1,2,3,4,5,5,6]));
